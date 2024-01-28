0db17bc261c180278507963c1ebf634bc5d6c9f8
mast3rm1nd <mast3r.b10ta@protonmail.com>
2024-01-28 14:09:07 +0100

    adding Code to the Challenge

--- src/PlayingCat.java
+++ src/PlayingCat.java
@@ -1,5 +1,17 @@
 public class PlayingCat {
     public static void main(String[] args) {
-
+        System.out.println(isCatPlaying(false,34));
     }
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        boolean isPlaying = true;
+        if (summer && temperature >= 25 && temperature <= 45 ) {
+            isPlaying = true;
+        } else  if (!summer && temperature >= 25 && temperature <= 35 ){
+            isPlaying = true;
+        } else {
+            isPlaying = false;
+        }
+        return isPlaying;
+    }
+
 }
