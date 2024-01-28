6724809f21719c7e2bc5c61e586a7f60cb308323
mast3rm1nd <mast3r.b10ta@protonmail.com>
2024-01-28 21:06:25 +0100

    adding Code to the Challenge adding new Files

--- src/NumberOfDaysInMonth.java
+++ src/NumberOfDaysInMonth.java
@@ -1,7 +1,7 @@
 public class NumberOfDaysInMonth {
 
     public static void main(String[] args) {
-        System.out.println(getDaysInMonth(-2,2020));
+        System.out.println(getDaysInMonth(-2, 2020));
     }
 
     public static boolean isLeap(int year) {
@@ -21,21 +21,20 @@ public class NumberOfDaysInMonth {
     }
 
     public static int getDaysInMonth(int month, int year) {
-       boolean isLeap = isLeap(year);
-       if (month < 1 || month > 12 || year < 1 || year > 9999) {
-           return -1;
+        boolean isLeap = isLeap(year);
+        if (month < 1 || month > 12 || year < 1 || year > 9999) {
+            return -1;
 
-       }
-       if (month == 2 && isLeap )
-       {
-           return 29;
-       }
-       return switch (month) {
-           case 1,3,5,7,8,10,12 -> 31;
-           case 4,6,9,11 -> 30;
-           case 2 -> 28;
-           default -> -1;
-       };
+        }
+        if (month == 2 && isLeap) {
+            return 29;
+        }
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            case 2 -> 28;
+            default -> -1;
+        };
     }
 
 }
