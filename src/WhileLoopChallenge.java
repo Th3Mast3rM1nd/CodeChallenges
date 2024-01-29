eda97c24cf3db0d238005ae676ededd84451d24a
mast3rm1nd <mast3r.b10ta@protonmail.com>
2024-01-29 21:13:45 +0100

    adding Code to the Challenge adding new Files

--- src/WhileLoopChallenge.java
+++ src/WhileLoopChallenge.java
@@ -1,13 +1,25 @@
+import java.io.FileOutputStream;
+
 public class WhileLoopChallenge {
     public static void main(String[] args) {
         int start = 4;
         int end = 20;
+        int foundEven = 0;
+        int foundOdd = 0;
         while (start <= end) {
             if (isEvenNumber(start)) {
                 System.out.println(start);
+                foundEven++;
+                if (foundEven == 5) {
+                    break;
+                }
+            } else {
+                foundOdd++;
             }
             start++;
         }
+       System.out.println("Total Even number Found is " + foundEven);
+        System.out.println("Total Even number Found is " + foundOdd);
     }
 
     public static boolean isEvenNumber(int number) {
